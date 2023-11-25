a2510d76de699853f177195cd541874c0a953577
HarshKrishan <krishanharsh82@gmail.com>
2023-11-25 15:52:23 +0530

    edited connectDb

--- src/app/api/connectDb/route.js
+++ src/app/api/connectDb/route.js
@@ -1,30 +1,31 @@
 import mysql from 'mysql2';
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  database: "mydatabase",
-  user: "webadmin",
-  password: "Web@2023",
-});
-const connectSql = ()=>{
-    try{
-        connection.connect(function(err) {
-            if (err) throw err;
-            console.log("Connected!");
-        });
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   database: "mydatabase",
+//   user: "webadmin",
+//   password: "Web@2023",
+// });
+// const connectSql = ()=>{
+//     try{
+//         connection.connect(function(err) {
+//             if (err) throw err;
+//             console.log("Connected!");
+//         });
 
-    }catch(error){
-        console.log("error connecting sql",error)
-    } 
+//     }catch(error){
+//         console.log("error connecting sql",error)
+//     } 
    
-}
+// }
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
-
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
+const connectSql = null;
+const connection = null;
 export default connectSql;
 export {connection};
