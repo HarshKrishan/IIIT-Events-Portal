f59ceb3767f87926450fb1a36b340d5d60c61130
HarshKrishan <krishanharsh82@gmail.com>
2023-11-25 15:55:17 +0530

    updated getEventImages

--- src/app/api/getEventImages/route.js
+++ src/app/api/getEventImages/route.js
@@ -3,20 +3,42 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
     // console.log("entering getAllEvents route");
-    const {eventId} = await req.json();
-    connectSql();
-    const query = `select * from images where Events_eventId = '${eventId}'`;
-    const images = await connection.promise().query(query).then(([data,fields]) => {
-        // console.log(data);
-        return data;
-        
-    }).catch((err) => {
-        console.log(err);
-    }
-    );
 
-    return NextResponse.json(
-        {result:images},
-        {status:200},
-    );
+
+    const {eventId} = await req.json();
+
+
+    //for local sql
+    // connectSql();
+    // const query = `select * from images where Events_eventId = '${eventId}'`;
+    // const images = await connection.promise().query(query).then(([data,fields]) => {
+    //     // console.log(data);
+    //     return data;
+        
+    // }).catch((err) => {
+    //     console.log(err);
+    // }
+    // );
+
+    // return NextResponse.json(
+    //     {result:images},
+    //     {status:200},
+    // );
+
+    //for vercel sql
+    const client = createClient();
+    await client.connect();
+
+    try {
+        const { rows, fields } = await client.sql`select * from images where Events_eventId = ${eventId};`;
+        return NextResponse.json({ result: rows }, { status: 200 });
+
+    } catch (error) {
+        console.log("error connecting sql", error)
+    }
+    finally {
+        await client.end();
+    }
+
+    return NextResponse.json({ result: "Error getting images" }, { status: 200 });
 }
