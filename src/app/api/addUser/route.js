0a8767713ab516aeee210f2b5f98a27f6527b8fe
HarshKrishan <krishanharsh82@gmail.com>
2023-11-26 16:55:21 +0530

    added force-dynamic to addUser api

--- src/app/api/addUser/route.js
+++ src/app/api/addUser/route.js
@@ -3,6 +3,8 @@
 import { NextResponse } from "next/server";
 import { createClient } from '@vercel/postgres'
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req) {
   console.log("entering addUser route");
   // console.log(req);
