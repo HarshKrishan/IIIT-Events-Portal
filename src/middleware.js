9de0a6eb3a6fffc53b9a08ba51d81dc6046a466e
HarshKrishan <krishanharsh82@gmail.com>
2023-11-28 23:03:29 +0530

    improved middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -2,9 +2,11 @@ import { NextResponse } from "next/server";
 
 
 export function middleware(request) {
-
+    if(request.path.url==='/api/getAllEvents'){
+        return NextResponse.next();
+    }
   return NextResponse.redirect(new URL("/login", request.url));
 }
 export const config = {
-  matcher: ["/dashboardAdmin", "/manageUsers"],
+  matcher: ["/dashboardAdmin", "/manageUsers","/api/:path*"],
 };
