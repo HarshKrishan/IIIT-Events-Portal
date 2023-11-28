b6f8922c9757ae8cee828fe5ad508bca08981f30
HarshKrishan <krishanharsh82@gmail.com>
2023-11-28 23:18:48 +0530

    edited middleware

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ export function middleware(request) {
     if(request.nextUrl.pathname==='/api/getAllEvents' || request.nextUrl.pathname.startsWith('/api/auth/')){
         return;
     }
-  return NextResponse.redirect(new URL("/login", request.url));
+//   return NextResponse.redirect(new URL("/login", request.url));
 }
 export const config = {
   matcher: ["/dashboardAdmin", "/manageUsers","/api/:path*"],
