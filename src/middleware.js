0e455b546b53c71e00723ee10bc89ec0400f2449
HarshAggarwal1 <haggarwal2510@gmail.com>
2023-11-29 11:40:35 +0530

    reverted back to previous code

--- src/middleware.js
+++ src/middleware.js
@@ -17,5 +17,5 @@ export function middleware(request) {
   return NextResponse.redirect(new URL("/login", request.url));
 }
 export const config = {
-  matcher: ["/dashboardAdmin", "/manageUsers","/api/*"],
+  matcher: ["/dashboardAdmin", "/manageUsers","/api/:path*"],
 };
